189a6e9a217956251d74c5d844a383973ec1e98d
靳广宇 <jinguangyu@jinguangyudeMacBook-Pro.local>
2018-07-30 18:21:51 +0800

    增加数组去重

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview  方法集
- * @author        guangyu jin
+ * @author        GuangYu Jin
  * @date          2018-04-10
  * @directory     父子页面交互
  */
@@ -8,6 +8,19 @@
 /**
  * 父子页面交互
  */
+
 window.open('sUrl', 'sName', 'sFeature', 'bReplace'); // 父页面
 
 window.opener.document.getElementById('xx'); // 子页面获取父元素
+
+/**
+* 数组去重
+* */
+
+// 1
+[...new Set(array)]
+
+// 2
+function dedupe (array) {
+  return Array.from(new Set(array))
+}
