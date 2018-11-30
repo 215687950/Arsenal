86b3949eabc59e9365dfadb867c947083174e3d5
靳广宇 <jinguangyu@jinguangyudeMacBook-Pro.local>
2018-11-30 14:06:13 +0800

    deepcopy

--- utils.js
+++ utils.js
@@ -17,10 +17,34 @@ window.opener.document.getElementById('xx'); // 子页面获取父元素
 * 数组去重
 * */
 
-// 1
 [...new Set(array)]
 
-// 2
 function dedupe (array) {
   return Array.from(new Set(array))
+}
+
+/*
+* 深拷贝
+* 递归 -- 未考虑死循环
+*/
+function deepCopy(data) {
+  let t = typeof(data)
+  let o
+  if (t === 'array') {
+    t = []
+  } else if (t === 'object') {
+    t = {}
+  } else {
+    return data
+  }
+  if (t === 'array') {
+    for (let i; i < data.length; i++) {
+      o.push(deepCopy(data[i]))
+    }
+  } else if (t === 'object') {
+    for (let i in data) {
+      o[i] = deepCopy(data[i])
+    }
+  }
+  return o
 }
